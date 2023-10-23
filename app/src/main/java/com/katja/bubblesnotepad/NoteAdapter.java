d525d66a6dd7578a6e65bb9c81a7bdd24d556904
Katja <katja@klahr.se>
2023-10-23 14:22:22 +0200

    Storlek på bubblor fixad.

--- app/src/main/java/com/katja/bubblesnotepad/NoteAdapter.java
+++ app/src/main/java/com/katja/bubblesnotepad/NoteAdapter.java
@@ -1,6 +1,7 @@
 package com.katja.bubblesnotepad;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
